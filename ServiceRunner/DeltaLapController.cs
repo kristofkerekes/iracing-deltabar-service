f31eb9856139fcdf390bc6232d423cb08bda6cbc
Kristóf Kerekes <kristof.kerekes@shapr3d.com>
2022-05-25 22:49:08 +0200

    Delta Service Initial

---
+++ ServiceRunner/DeltaLapController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ServiceRunner {
+	public class DeltaLapController {
+		private DeltaLapStorage _deltaStorage;
+		private iRacingDeltaLapStorage _iRacingStorage;
+
+		public DeltaLapController(DirectoryInfo deltaStorageInfo, DirectoryInfo iRacingStorageInfo) {
+			_deltaStorage = new DeltaLapStorage(deltaStorageInfo);
+			_iRacingStorage = new iRacingDeltaLapStorage(iRacingStorageInfo);
+		}
+
+		void ExportLapsFromSimulator(DriverCarInfo) {
+			_iRacingStorage.Find();
+		}
+	}
+}
---
+++ ServiceRunnerDotNet/DeltaLapController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ServiceRunner {
+	public class DeltaLapController {
+		private DeltaLapStorage _deltaStorage;
+		private iRacingDeltaLapStorage _iRacingStorage;
+
+		public DeltaLapController(DirectoryInfo deltaStorageInfo, DirectoryInfo iRacingStorageInfo) {
+			_deltaStorage = new DeltaLapStorage(deltaStorageInfo);
+			_iRacingStorage = new iRacingDeltaLapStorage(iRacingStorageInfo);
+		}
+
+		bool ExportLapsFromSimulator(iRacingSimulator.Sim iRacingInstance) {
+			string trackId = iRacingInstance.SessionData.Track.CodeName;
+			string carId = iRacingInstance.Driver.Car.CarPath;
+			DeltaLaps? laps = _iRacingStorage.Find(trackId, carId);
+			if (laps == null) {
+				return false;
+			}
+
+			string seriesId = iRacingInstance.SessionInfo["WeekendInfo"]["SeriesID"].GetValue();
+			uint raceWeek = uint.Parse(iRacingInstance.SessionInfo["WeekendInfo"]["RaceWeek"].GetValue());
+			return _deltaStorage.Insert(seriesId, raceWeek, (DeltaLaps)laps);
+		}
+	}
+}
